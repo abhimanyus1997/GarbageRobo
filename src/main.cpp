65f34bc05a4040ff17839cbf7408f8033a0779ea
Abhimanyu <abhimanyus1997@gmail.com>
2021-11-27 22:07:50 +0530

    Motor Direction control with BT

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,12 @@
 #include <BluetoothSerial.h>
 
 #define ledPIN 2
+
+int in1 = 13; // Motor A connections
+int in2 = 12;
+int in3 = 14; // Motor B connections
+int in4 = 27;
+
 BluetoothSerial SerialBT;
 byte BTData;
 
@@ -13,31 +19,88 @@ byte BTData;
 void setup()
 {
   pinMode(ledPIN, OUTPUT);
-  Serial.begin(115200);
+  Serial.begin(9600);
   SerialBT.begin();
-  Serial.println("Bluetooth Started! Ready to pair...");
+  //Serial.println("Bluetooth Started! Ready to pair...");
+
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  // Turn off motors - Initial state
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
 }
 
 void loop()
 {
-  if(SerialBT.available())
+  if (SerialBT.available())
   {
     BTData = SerialBT.read();
     Serial.write(BTData);
   }
+  char Data = (char)BTData;
 
-  /* If received Character is 1, then turn ON the LED */
-  /* You can also compare the received data with decimal equivalent */
-  /* 48 for 0 and 49 for 1 */
-  /* if(BTData == 48) or if(BTData == 49) */
-  if(BTData == '1')
+  if (Data == 'w')
   {
-    digitalWrite(ledPIN, HIGH);
+    // Now turn FWD motors
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
   }
-  
-  /* If received Character is 0, then turn OFF the LED */
-  if(BTData == '0')
+
+  else if (Data == 's')
   {
-    digitalWrite(ledPIN, LOW);
+    // Now turn FWD motors
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+  }
+
+  else if (Data == 'a')
+  {
+    // Now left motors
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+  }
+    else if (Data == 'A')
+  {
+    // Now left motors
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+  }
+
+  else if (Data == 'd')
+  {
+    // Now right motors
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
+  }
+  else if (Data == 'D')
+  {
+    // Now right motors
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+  }
+
+  else
+  {
+    // Now stop motors
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
   }
 }
